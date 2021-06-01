b51572a01524b292f08ea687dd290a15be222b12
eNji1337 <antonyan1337@gmail.com>
2021-06-01 15:54:01 +0300

    HomeWork

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        System.out.println();
+        checkSumSign();
+        System.out.println();
+        printColor();
+        System.out.println();
+        compareNumbers();
+    }
+    public static void printThreeWords() {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 2;
+        int b = 2;
+        int c = b + a;
+        if (c >= 0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+    public static void printColor() {
+        int value = 11;
+        if (value <= 0)
+            System.out.println("Красный");
+        else if (value > 0 && value <= 100)
+            System.out.println("Желтый");
+        else
+            System.out.println("Зеленый");
+    }
+    public static void compareNumbers() {
+        int b = 3;
+        int a = 2;
+        if (a >= b)
+            System.out.println("a>=b");
+        else
+            System.out.println("a<b");
+    }
+}
