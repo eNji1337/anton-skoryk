622d84be5f83e4fea81d70ad955b512e812b8066
eNji1337 <80171056+eNji1337@users.noreply.github.com>
2021-06-04 10:09:06 +0300

    Task3

---
+++ src/main/java/org/example/task3/Cat.java
@@ -0,0 +1,8 @@
+package org.example.task3;
+
+class Cat extends Animal {
+
+    Cat(double animalDistanceRun, double animalDistanceSwim) {
+        super(animalDistanceRun, animalDistanceSwim);
+    }
+}
---
+++ src/main/java/org/example/task3/Dog.java
@@ -0,0 +1,9 @@
+package org.example.task3;
+
+class Dog extends Animal {
+
+    Dog(double animalDistanceRun, double animalDistanceSwim) {
+        super(animalDistanceRun, animalDistanceSwim);
+
+    }
+}
