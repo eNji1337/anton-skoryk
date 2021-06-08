fe9631b72098e1d2688c7dabe067c3844d318e5b
eNji1337 <80171056+eNji1337@users.noreply.github.com>
2021-06-08 14:03:45 +0300

    Task7

---
+++ src/main/java/Task7_2/Fruit.java
@@ -0,0 +1,13 @@
+package Task7_2;
+
+public abstract class Fruit {
+    protected float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/Task7_2/Orange.java
@@ -0,0 +1,7 @@
+package Task7_2;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5f);
+    }
+}
